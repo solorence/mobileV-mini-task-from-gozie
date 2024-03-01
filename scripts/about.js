03ffa8788756b36c370145636cc1065bcc5eea3b
solorence <doc.electro1@gmail.com>
2024-03-01 20:58:37 +0100

    update on page links with Javascript

---
+++ scripts/about.js
@@ -0,0 +1,30 @@
+let home = document.querySelector('#home');
+
+let about = document.querySelector('#about');
+
+let contactus = document.querySelector('#contact');
+
+let homeLink = '../index.html';
+
+let aboutLink = '../about.html';
+
+let contactLink = '../find.html';
+
+
+home.addEventListener('click', toHome);
+
+about.addEventListener('click', toAbout);
+
+contactus.addEventListener('click', toContactus);
+
+function toHome() {
+  location.href = homeLink;
+}
+
+function toAbout() {
+  location.href = aboutLink;
+}
+
+function toContactus() {
+  location.href = contactLink;
+}
--- scripts/contact.js
+++ scripts/contact.js
@@ -26,6 +26,37 @@ submit.addEventListener('click', formValidation);
 let form = document.querySelector('form');
 
 
+let home = document.querySelector('#home');
+
+let about = document.querySelector('#about');
+
+let contactus = document.querySelector('#contact');
+
+let homeLink ='../index.html';
+
+let aboutLink ='../about.html';
+
+let contactLink ='../find.html';
+
+
+home.addEventListener('click', toHome);
+
+about.addEventListener('click', toAbout);
+
+contactus.addEventListener('click', toContactus);
+
+function toHome () {
+  location.href = homeLink;
+}
+
+function toAbout() {
+  location.href = aboutLink;
+}
+
+function toContactus() {
+  location.href = contactLink;
+}
+
 
 function formValidation(event) {
   event.preventDefault();
